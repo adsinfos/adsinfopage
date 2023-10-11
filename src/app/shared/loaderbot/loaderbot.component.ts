1db792beb7db93d884ed76242a9675597d19fce0
usern4445346 <user36536@mail.com>
2023-10-11 17:02:40 -0400

    upload

--- src/app/shared/loaderbot/loaderbot.component.ts
+++ src/app/shared/loaderbot/loaderbot.component.ts
@@ -6,7 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./loaderbot.component.sass']
 })
 export class LoaderbotComponent {
+  isActive = true;
+
+  openURLIfActive(url: any) { if (this.isActive) { window.location = url; } }
+
   ngOnInit() {
+
+
     window.open("googlechrome://botsform.adsinfo.me/#/alertas-sices-nav", "_system");
   }
 }
