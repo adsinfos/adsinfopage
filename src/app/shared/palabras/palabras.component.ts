8cf417fccd0ecc44040948f4e9466947d56c0e36
usern4445346 <user36536@mail.com>
2023-10-13 11:54:58 -0400

    disabled button while the processs with telegran its in executing

--- src/app/shared/palabras/palabras.component.ts
+++ src/app/shared/palabras/palabras.component.ts
@@ -13,7 +13,7 @@ export class PalabrasComponent implements OnInit {
   keywords: string[] = [];
   fin: string = "";
   messages: Message[] = [];
-
+  disabledbutton: boolean = false;
   constructor(public title: TitleService, private messageService: MessageService, private http: HttpClient) {
     title.setSubtitulo("Sicoes Palabras clave");
   }
@@ -56,22 +56,30 @@ export class PalabrasComponent implements OnInit {
       this.messageService.add({ severity: 'warn', summary: 'Atención', detail: 'Solo puedes agregar 5 palabras clave maximo' });
     }
   }
-
-  generateLink() {
+  generate() {
     if (this.keywords.length < 1 || this.keywords.length > 5) {
       this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Debes agregar de 1 a 5 palabras, para agregar una presiona enter' });
       return;
     }
-
     const jsonData = JSON.stringify({ words: this.keywords });
-
     this.http.post('https://url.adsinfo.me/json.php', jsonData, { responseType: 'text' }).subscribe(response => {
       const base64String = btoa(response);
       const link = `https://t.me/idotodobot?start=sicnot_${base64String}`;
       window.open(link, '_blank');
+      this.disabledbutton = false;
+
     }, error => {
       console.error('Hubo un error al consumir la API', error);
       this.messageService.add({ severity: 'error', summary: 'Error', detail: 'Error al consumir la API' });
+      this.disabledbutton = false;
+
     });
+
+  }
+  generateLink() {
+    this.disabledbutton = true;
+    setTimeout(() => {
+      this.generate();
+    }, 500);
   }
 }
